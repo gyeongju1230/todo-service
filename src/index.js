ae80971b6119fa0a7428df5b5eea3e4cb70f5a91
zoodaa <jkj01230@gmail.com>
2024-03-12 01:17:55 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App />, errorElement: <NotFound /> },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);

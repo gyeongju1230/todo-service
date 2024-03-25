77aac7e6aba64553ac581857ad0c5dcd13c19a9f
zoodaa <jkj01230@gmail.com>
2024-03-25 22:26:42 +0900

    feat: sidebar 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Navbar from "./pages/navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import TodoList from "./pages/todo-list/TodoList";
+import { useState } from "react";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <>
-      <Navbar />
+      <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <Outlet />
+      <TodoList sidebarOpen={sidebarOpen} />
     </>
   );
 }

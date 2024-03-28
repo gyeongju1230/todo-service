a5959d02df7ced64bf7f760e3584ff3aef8c6e12
zoodaa <jkj01230@gmail.com>
2024-03-28 15:43:52 +0900

    refactor: Todo 예시 data 수정

--- src/data/TodoExample.js
+++ src/data/TodoExample.js
@@ -1,23 +1,23 @@
 export const TodoExample = {
   section1: {
-    title: "유산소 운동하기🏃",
-    content: "1시간 이상 유산소 운동 꼭 하기! 🙌🏻",
-  },
-  section2: {
-    title: "책 읽기 📚",
-    content: "한달에 2권은 필수!",
-  },
-  section3: {
-    title: "여행 계획세우기!",
-    content: "✅ 공항에서 호텔까지 소요시간 확인!",
-  },
-  section4: {
     title: "개발공부 👩🏻‍💻",
     content: "개발 공부는 꾸준히 매일 하자! ✨",
   },
-  section5: {
+  section2: {
     title: "기술동향 확인",
     content: "💡 프론트엔드 최신 기술 확인!",
+  },
+  section3: {
+    title: "책 읽기 📚",
+    content: "한달에 2권은 필수!",
+  },
+  section4: {
+    title: "유산소 운동하기🏃",
+    content: "1시간 이상 유산소 운동 꼭 하기! 🙌🏻",
+  },
+  section5: {
+    title: "여행 계획세우기!",
+    content: "공항에서 호텔까지 소요시간 확인! ⏰",
   },
   section6: {
     title: "블로그 작성 📝",

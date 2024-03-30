eced5395241ca704b37ca91e43daaf2755e790be
zoodaa <jkj01230@gmail.com>
2024-03-30 22:56:56 +0900

    feat: 다크모드 color 정의

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const dark = {
+  colors: {
+    bgColor: "#CFCFCF",
+    titleColor: "#ffffff",
+  },
+};
+
+export const light = {
+  colors: {
+    bgColor: "#ffffff",
+    titleColor: "#8c8c8c",
+  },
+};
